8fa1847185d53b2e60731dbca5f5d49285e93a0c
Eyrian Muet <eyrianmuet@proton.me>
2026-04-17 16:45:39 +1100

    Better UI/UX + reload button

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,30 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const pixels = [];
+
     // Get HTML elements
     const pixelsList = document.getElementById('pixelsList');
     const pixelTemplate = document.getElementById('pixelTemplate');
+    const reloadPixelsButton = document.getElementById('reloadPixels');
   
     // Add a pixel to the list
     function addPixel(pixel) {
         const pixelElement = pixelTemplate.content.cloneNode(true);
         pixelElement.querySelector('.pixelRow').id = `pixel-${pixel.id}`;
         pixelElement.querySelector('.pixelLabel').textContent = pixel.label;
-        pixelElement.querySelector('.pixelCreationDate').textContent = pixel.creationDate;
+        pixelElement.querySelector('.pixelCreationDate').textContent = `Created : ${pixel.creationDate}`;
         pixelElement.querySelector('.pixelReadCount').textContent = pixel.readCount;
-        pixelElement.querySelector('.pixelLastRead').textContent = pixel.lastRead;
+        pixelElement.querySelector('.pixelLastRead').textContent = pixel.lastRead ? `Last read : ${pixel.lastRead}` : 'Not read yet';
         pixelElement.querySelector('.deletePixel').dataset.pixelId = pixel.id;
         pixelsList.appendChild(pixelElement);
     }
 
     // Fetch pixels from the server
-    fetch(`${CONFIG.API_URL}/pixels`, {
-        headers: { 'X-API-Key': CONFIG.API_KEY }
-    })
-    .then(response => {
-        if (!response.ok) throw new Error(`Server error: ${response.status}`);
-        return response.json();
-    })
-    .then(data => {
-        data.forEach(pixel => {
+    function fetchPixels() {
+        reloadPixelsButton.disabled = true;
+        fetch(`${CONFIG.API_URL}/pixels`, {
+            headers: { 'X-API-Key': CONFIG.API_KEY }
+        })
+        .then(response => {
+            if (!response.ok) throw new Error(`Server error: ${response.status}`);
+            return response.json();
+        })
+        .then(data => {
+            pixels.length = 0;
+            data.forEach(pixel => {
+                pixels.push(pixel);
+            });
+            renderPixels();
+        })
+        .catch(err => {
+            // Display error message
+            const error = document.createElement('p');
+            error.id = 'fetchError';
+            error.textContent = err.message.includes('Failed to fetch')
+                ? 'Cannot reach the server.'
+                : err.message;
+            document.getElementById('content').appendChild(error);
+        })
+        .finally(() => {
+            setTimeout(() => {
+                reloadPixelsButton.disabled = false;
+                reloadPixelsButton.querySelector('svg').classList.remove('loading');
+            }, 500);
+        });
+    }
+    
+    // Render pixels in the list
+    function renderPixels() {
+        pixelsList.innerHTML = '';
+        pixels.forEach(pixel => {
             addPixel({
                 id: pixel.id,
                 label: pixel.label,
@@ -33,25 +64,18 @@ document.addEventListener('DOMContentLoaded', function () {
                 lastRead: pixel.last_read_at ? new Date(pixel.last_read_at).toLocaleString('en-EN', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' }) : '',
             });
         });
-    })
-    .catch(err => {
-        // Display error message
-        const error = document.createElement('p');
-        error.id = 'fetchError';
-        error.textContent = err.message.includes('Failed to fetch')
-            ? 'Cannot reach the server.'
-            : err.message;
-        document.getElementById('content').appendChild(error);
-    });
+    }
+    
+    // Load pixels when the popup opens
+    fetchPixels();
     
     // Add event listener for delete buttons
     document.addEventListener('click', function(e) {
-        console.log(e.target);
         if (e.target.classList.contains('deletePixel')) {
             const pixelId = e.target.dataset.pixelId;
-            console.log('Pixel ID:', pixelId);
             
             // Fetch pixel deletion
+            e.target.disabled = true;
             fetch(`${CONFIG.API_URL}/pixels/${pixelId}`, {
                 method: 'DELETE',
                 headers: { 'X-API-Key': CONFIG.API_KEY }
@@ -61,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 return response.json();
             })
             .then(() => {
-                pixelsList.removeChild(document.getElementById(`pixel-${pixelId}`));
+                fetchPixels();
             })
             .catch(err => {
                 // Display error message
@@ -71,7 +95,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     ? 'Cannot reach the server.'
                     : err.message;
                 document.getElementById('content').appendChild(error);
-            });
+                e.target.disabled = false;
+            })
         }
+    });
+
+    // Event listener for the reload button
+    reloadPixelsButton.addEventListener('click', function() {
+        reloadPixelsButton.querySelector('svg').classList.add('loading');
+        fetchPixels();
     });
 });
